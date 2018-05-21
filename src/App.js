add4fcab7ae169cdfb46007f8ffe3e36cf87a271
ellinorjn <ellinor.jansson@medieinstitutet.se>
2018-05-21 13:19:23 +0200

    login with username

--- src/App.js
+++ src/App.js
@@ -3,14 +3,42 @@ import './App.css';
 import Username from './Username';
 
 class App extends Component {
+
+  state = {
+    loggedIn: false,
+    username: ''
+  }
+
+  handleLogin = (username) => {
+    this.setState({ loggedIn: true, username: username });
+  }
+
   render() {
+    let loggedInMessage = null;
+    if(this.state.loggedIn){
+      loggedInMessage = <p> Welcome {this.state.username}, let's play! </p>
+    }
+
+
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        { loggedInMessage }
+        {
+          /**
+           * This is a shorthand for displaying or hiding a component,
+           * if 'loggedIn' is false the next part of the if-statement
+           * cannot continue and the component will not show. This is the same as 
+           * saying true && true or false && false. Both must be true for the
+           * component to show.
+           */
+          !this.state.loggedIn && <Username handleLogin={this.handleLogin} />
+        }
+    
+
         
-        <Username />
 
       </div>
     );
--- src/Username.js
+++ src/Username.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 
 class Username extends Component{
+    state = {
+        username: ''
+    }
+
+    handleUsername = (event) => {
+        this.setState({ username: event.target.value });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        if(this.state.username.length < 5 ){
+            this.setState( {error: "Username too short"} )
+        }else{
+            this.props.handleLogin(this.state.username);
+        }
+    }
+
     render(){
         return(
-            <form>
-                <label htmlFor="username">Username</label>
-                <input type="text" id="username"/>
+            <form onSubmit={this.handleSubmit}>
+                <div className="form-group">
+                    <label forhtml="username">Username</label>
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        id="username" 
+                        placeholder="Username" 
+                        onChange={this.handleUsername}
+                        value={this.state.username}/>
+                </div>
                 <button type="submit" className="btn btn-primary">Go!</button>
             </form>
         );
